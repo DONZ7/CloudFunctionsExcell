5de7ae7ca90157445931077cd7135027efe4f3c9
Alejandro Zelaya <jandro.zg123@gmail.com>
2020-08-06 21:06:13 -0600

    cambios en el cogigo y su estructura

--- functions/index.js
+++ functions/index.js
@@ -1,94 +1,38 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-const express=require('express');
-const elim = express();
-const bd=admin.firestore()
-
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const serviceAccount = require("./excell-33b13-firebase-adminsdk-9atg4-0eb46d0544.json");
 const cors = require("cors")({ origin: true });
 
-// funcion visualizar
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://excell-33b13.firebaseio.com",
+});
 
-exports.getProductos =functions.https.onRequest((req, res) =>
+const db = admin.firestore();
 
-cors(req, res, async () => {
-  try {
-  let productos= {data:[]};
-  produtos.snapshot= await bd.collection('Productos')
- // orderBy('fechaCreado','desc')
-  .get()
-  produtos.snapshot.forEach((doc)=> {
-      productos.data.push({    
-        id: doc.id, 
-        ...doc.data()
+exports.setProductos = functions.https.onRequest((req, res) =>
+  cors(req, res, async () => {
+    try {
+      const { token, data } = req.body;
+      const { uid } = await admin.auth().verifyIdToken(token);
+      if(!uid){
+        res.status(400).json({
+          success: false,
+          messange: "Error auth",
+        });
       }
-      );
-    });
-  res.status(200).json({
-   data: productos.data,
-  success:true,
-  messange:"datos exitoso"} )  
-  } catch (error) {
-    res.status(500).json({
-      data: [],
-     success:false,
-     messange:"errr de servicio"} ) 
-    
-  }
-})
-);
-
-
-
-
-exports.postProductos = functions.https.onRequest((req, res) =>
-cors(req, res, async ( ) => {
-  try {
-const {informacion}=req.body
-await bd.collection('Productos')
-//.orderBy('nombre','desc')
-.add(informacion)
-res.status(200).json({
-messange:"datos exitoso"} ) 
-}
-catch(error) {
-   res.status(500).json({
-     data: [],
-    success:false,
-    messange:"errr de servicio"} ) 
-  }
-})
-);
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
- exports.add = functions.https.onRequest((req,res)=> {
-     res.send('hola mundo') ;
- });
-
-
-
- exports.postLugares = functions.https.onRequest((req, res) =>
-cors(req, res, async ( ) => {
-  try {
-const {informacion}=req.body
-await bd.collection('lugares')
-//.orderBy('nombre','desc')
-.add(informacion)
-res.status(200).json({
-messange:"datos exitoso"} ) 
-}
-catch(error) {
-   res.status(500).json({
-     data: [],
-    success:false,
-    messange:"errr de servicio"} ) 
-  }
-})
+      for(let i in data){
+        await db.collection("Productos").add(data[i]);
+      }
+      res.status(200).json({
+        success: true,
+        messange: "datos introducidos exitosamente",
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        messange: "Error server",
+      });
+    }
+  })
 );
