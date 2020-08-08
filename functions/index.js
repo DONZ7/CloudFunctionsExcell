7314dedabc57f806bf09551d2406b25dcb460295
Alejandro Zelaya <jandro.zg123@gmail.com>
2020-08-07 19:42:55 -0600

    functions ready

--- functions/index.js
+++ functions/index.js
@@ -10,28 +10,47 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
+const discompose = (name) => {
+  let response=[]
+  let str=name.toLowerCase().normalize('NFD')
+  .replace(/([aeio])\u0301|(u)[\u0301\u0308]/gi,"$1$2")
+  .normalize();
+  let arrayName = str.split(" ");
+  let newStr=str
+  for(let i in arrayName){
+      newStr=newStr.replace(" ","")
+  }
+  arrayName.push(newStr)
+  arrayName.shift()
+  for(let i in arrayName){
+      let h=1;
+      for(let j in arrayName[i]){
+          response.push(arrayName[i].substr(0,h))
+          h=h+1;
+      }
+  }
+  return response;
+};
+
 exports.setProductos = functions.https.onRequest((req, res) =>
   cors(req, res, async () => {
     try {
-      const { token, data } = req.body;
-      const { uid } = await admin.auth().verifyIdToken(token);
-      if(!uid){
-        res.status(400).json({
-          success: false,
-          messange: "Error auth",
+      const { products, country } = req.body;
+      for(let i in products){
+        let caseSearch = discompose(products[i].name);
+        await db.collection("countries").doc(country).collection("products").add({
+          ...products[i],
+          caseSearch: caseSearch,
         });
-      }
-      for(let i in data){
-        await db.collection("Productos").add(data[i]);
       }
       res.status(200).json({
         success: true,
-        messange: "datos introducidos exitosamente",
+        message: "Datos introducidos exitosamente",
       });
     } catch (error) {
       res.status(500).json({
         success: false,
-        messange: "Error server",
+        message: "Error server",
       });
     }
   })
